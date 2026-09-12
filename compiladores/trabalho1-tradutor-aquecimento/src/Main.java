4efdbe2bfd1f6c8372f59e32e00b22356f005d26
higorcos <higorcosta2001@outlook.com>
2026-09-12 16:05:24 -0300

    Trabalho 1: refatora a inicialização do Scanner para usar uma constante SOURCE

--- compiladores/trabalho1-tradutor-aquecimento/src/Main.java
+++ compiladores/trabalho1-tradutor-aquecimento/src/Main.java
@@ -2,8 +2,10 @@ import java.util.List;
 
 public class Main {
 
+    private static final String SOURCE = "let a = 42 + 5; print a + 6; let b = a * 2 - 3; print b / 3;";
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner("let a = 42 + 5; print a + 6; let b = a * 2 - 3; print b / 3;");
+        Scanner scanner = new Scanner(SOURCE);
         Parser parser = new Parser(scanner);
         List<String> program = parser.translate();
 
