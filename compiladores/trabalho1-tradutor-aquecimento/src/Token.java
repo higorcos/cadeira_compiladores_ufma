25fa3ffa9db407db41ab69f4297b66e840b1dd2f
higorcos <higorcosta2001@outlook.com>
2026-09-12 16:06:12 -0300

    Trabalho 1: refatora a classe Token para usar um record, simplificando a definição

--- compiladores/trabalho1-tradutor-aquecimento/src/Token.java
+++ compiladores/trabalho1-tradutor-aquecimento/src/Token.java
@@ -1,12 +1,4 @@
-public class Token {
-
-    public final TokenType type;
-    public final String text;
-
-    public Token(TokenType type, String text) {
-        this.type = type;
-        this.text = text;
-    }
+public record Token(TokenType type, String text) {
 
     @Override
     public String toString() {
